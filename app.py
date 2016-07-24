dbd5a0f641cb5659706524f48ae79251f81c585d
shkiran4 <shkiran4@in.ibm.com>
2016-07-24 22:05:09 +0530

    more changes

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ import time
 # Define event callbacks
     
 def on_connect(mosq, obj, rc):
-    print "In the on_connect";
+    print ("on_connect:: Connected with result code "+ str ( rc ) )
     print("rc: " + str(rc))
 
 def on_message(mosq, obj, msg):
+    print ("on_message:: this means  I got a message from brokerfor this topic")
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 def on_publish(mosq, obj, mid):
     print("mid: " + str(mid))
 
 def on_subscribe(mosq, obj, mid, granted_qos):
+    print("This means broker has acknowledged my subscribe request")
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
 def on_log(mosq, obj, level, string):
@@ -32,17 +34,11 @@ client.on_log = on_log
 
 print "hello world"
 
-# Connect
+# user name has to be called before connect - my notes.
 client.username_pw_set("ghcqvpeu", "geZgGJ8tsPVk")
 print "before connecting"
 
 client.connect('m12.cloudmqtt.com', 12071, 60)
-print "after connecting"
-#while True :
-#        # Publish a message
-#        client.publish("/toclientloud", "from python code")
-#        time.sleep(5);
-
 
 # Continue the network loop, exit when an error occurs
 #rc = 0
@@ -54,4 +50,13 @@ print "after connecting"
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-client.loop_forever()
+#client.loop_forever()
+client.loop_start()
+client.subscribe ("/frommothership" ,0 )
+
+print "am I here "
+
+run = True
+while run:
+    client.publish ( "/toclientloud", "from python code")
+    time.sleep(1)
