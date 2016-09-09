5287e3c2b295fe216aa5464caf47c3cf544b09d9
shkiran4 <shkiran4@in.ibm.com>
2016-09-09 13:20:47 +0530

    added the URL

--- publish.py
+++ publish.py
@@ -13,16 +13,18 @@
 #    Roger Light - initial implementation
   
 # This shows an example of using the publish.single helper function.
+
+#http://nullege.com/codes/show/src%40p%40a%40paho-mqtt-0.9%40examples%40pub-single.py/32/paho.mqtt.publish.single/python
   
 import sys
 import paho.mqtt.publish as publish
 ### Single payload.
 #without payload.  
 publish.single("/fromlaptop", payload=None, qos=0, retain=False, hostname="m12.cloudmqtt.com",
-    port=19757, client_id="", keepalive=60, will=None, auth={'username':"oxefqvkn", 'password':"aKpQPSFiTpXp"}, tls=None)
+    port=19757, client_id="", keepalive=60, will=None, auth={'username':"XXXXX", 'password':"XXXX"}, tls=None)
 #with payload
 publish.single("/fromlaptop", payload="shashilaptop", qos=0, retain=False, hostname="m12.cloudmqtt.com",
-    port=19757, client_id="", keepalive=60, will=None, auth={'username':"oxefqvkn", 'password':"aKpQPSFiTpXp"}, tls=None)
+    port=19757, client_id="", keepalive=60, will=None, auth={'username':"XXXX", 'password':"xxxx"}, tls=None)
 
 
 # With multiple topics in a publish
@@ -33,4 +35,4 @@ msgs = [{'topic':"/fromLTMultiple", 'payload':"multiple 1"},
     ("/fromLTDifferenttopic", "multiple 2", 0, False)]
 
 publish.multiple(msgs, hostname="m12.cloudmqtt.com", port=19757, client_id="", keepalive=60,
-    will=None, auth={'username':"oxefqvkn", 'password':"aKpQPSFiTpXp"}, tls=None)
+    will=None, auth={'username':"XXXX", 'password':"XXXX"}, tls=None)
